ef24831f47cd9f0dc0226666f0a6aa773d5db6de
MK4Kard <misha212032@gmail.com>
2025-03-30 15:37:52 +0500

    Update EntPrise.kt

--- EntPrise.kt
+++ EntPrise.kt
@@ -1,13 +1,19 @@
 fun main(){
-    val One = Quotient("One", 140000.0, 125000.0)
+    println("Введите примерный и фактический объем дохода")
+    val One = Quotient("One", readln()!!.toDouble(), readln()!!.toDouble())
     One.Quality()
     One.Info()
     println()
-    val Two = DesQuo("Two", 140000.0, 125000.0)
+    println("Введите примерный и фактический объем дохода")
+    val Two = DesQuo("Two", readln()!!.toDouble(), readln()!!.toDouble())
     println("Введите процент налогов")
     Two.P(readln()!!.toInt())
     Two.Quality()
     Two.Info()
     Two.State()
-
+    println()
+    println("Введите дату регистрации")
+    val Comp = Company("COMP", true, readln()!!.toInt(), readln()!!.toInt(), readln()!!.toInt())
+    Comp.Registration()
+    Comp.Info()
 }
